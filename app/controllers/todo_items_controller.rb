0fee2292b245913203a5d5e50061e776e0a2a23a
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-01-31 00:42:09 +0700

    C to c (-_-)

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -17,7 +17,7 @@ class TodoItemsController < ApplicationController
     redirect_to @todo_list
   end
 
-  def Complete
+  def complete
     @todo_item.update_attribute(:completed_at, Time.now)
     redirect_to @todo_list, notice: "Todo item completed"
   end
